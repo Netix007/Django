f18427ec6281834fb13cf7eeda813c1833978c39
Alex <kino_mac@mail.ru>
2023-08-12 01:26:52 +0300

    Seminar4

--- Sem1/sem1/sem2_task1/views.py
+++ Sem1/sem1/sem2_task1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from sem2_task1.models import Author, Article
-from django.utils.safestring import mark_safe
+from sem2_task1.forms import AuthorForms, ArticleForms
+from django import forms
 import logging
 
 logger = logging.getLogger(__name__)
@@ -26,3 +27,43 @@ def get_article(request, article_id):
         'article': article
     }
     return render(request, template_name='sem2_task1/article.html', context=context)
+
+
+def add_author(request):
+    if request.method == 'POST':
+        form = AuthorForms(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            surname = form.cleaned_data.get('surname')
+            email = form.cleaned_data.get('email')
+            biography = form.cleaned_data.get('biography')
+            birthday = form.cleaned_data.get('birthday')
+
+            logger.info(f'New author: {name} {surname} {email} {biography} {birthday}')
+
+    else:
+        form = AuthorForms()
+    return render(request=request, template_name='sem2_task1/author_forms.html', context={'form': form})
+
+
+def add_article(request):
+    if request.method == 'POST':
+        form = ArticleForms(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            content = form.cleaned_data.get('content')
+            author = form.cleaned_data.get('author')
+            category = form.cleaned_data.get('category')
+
+            article = Article(
+                title=title,
+                content=content,
+                author=author,
+                category=category,
+            )
+            article.save()
+            logger.info(f'New article: {article}')
+
+    else:
+        form = ArticleForms()
+    return render(request=request, template_name='sem2_task1/article_forms.html', context={'form': form})
